f185beb75f864664305cf6f5e3147710f718e9a6
Mayank Saini <95845450+Mayanks08@users.noreply.github.com>
2025-07-04 11:45:57 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -3,46 +3,43 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import { config } from 'dotenv';
-import errorMiddleware from './middlewares/error-midlle.js'
-import CourseRoutes from './Routes/course-routes.js'
-import PaymentRoutes from './Routes/Payment-routes.js'
-import UserRoutes from './Routes/user-routes.js'
 
-config();
+import errorMiddleware from './middlewares/error-midlle.js';
+import CourseRoutes from './Routes/course-routes.js';
+import PaymentRoutes from './Routes/Payment-routes.js';
+import UserRoutes from './Routes/user-routes.js';
 
-const app = express()
+config(); 
+
+const app = express();
 
 
-
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
+  origin: process.env.FRONTEND_URL,  
+  credentials: true,
+}));
 
-}))
-
-app.use(cookieParser())
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended:true}));
-
-
-app.use('/api/v1/User', UserRoutes)
-app.use('api/v1/courses',CourseRoutes)
-app.use('api/v1/payments',PaymentRoutes)
-
-app.get('/',(req,res) =>{
-    res.send("Welcome to LMS Backend")
-    activeServer = true;
-
-})
-
-// routes of 3 modules
-app.all("*",(req,res) =>{
-    res.status(404).send("Route not found")
-})
-
-app.use(errorMiddleware)
+// Routes
+app.use('/api/v1/user', UserRoutes);
+app.use('/api/v1/courses', CourseRoutes);
+app.use('/api/v1/payments', PaymentRoutes);
 
 
-export default app
+app.get('/', (req, res) => {
+  res.send('Welcome to LMS Backend');
+});
+
+// 404 handler
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// Global error handler
+app.use(errorMiddleware);
+
+export default app;
